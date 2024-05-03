9bb17d10ab164e12824d6cc30a7fafe3065acf0c
Saurabh Singh <173saurabh@gmail.com>
2024-05-03 22:27:42 +0530

    shortest path --incomplete

---
+++ ShortestPath.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class ShortestPath {
+
+    public static void addEdge(ArrayList<ArrayList<Integer>> arr, int v, int u) {
+        arr.get(u).add(v);
+        arr.get(v).add(u);
+    }
+
+    public static void bfs() {
+
+    }
+
+    public static void bfsD(ArrayList<ArrayList<Integer>> arr, int v) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
