aab79ff4a4ef881bf460c13f50df31e5a0ba0223
Saurabh Singh <173saurabh@gmail.com>
2024-05-07 22:10:38 +0530

    cycle detection directed graph

---
+++ CycleDetectDG1.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class CycleDetectDG1 {
+
+    public static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v) {
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+
+    public static void main(String[] args) {
+        int v = 6;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < v; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        addEdge(adj, 0, 1);
+        addEdge(adj, 2, 1);
+        addEdge(adj, 2, 3);
+        addEdge(adj, 3, 4);
+        addEdge(adj, 4, 5);
+        addEdge(adj, 5, 3);
+
+    }
+}
